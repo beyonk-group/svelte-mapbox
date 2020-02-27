ce8b6523e9b89b2057fe4309212750d7e7c4f18a
Antony Jones <aj@desirableobjects.co.uk>
2020-02-27 15:34:20 +0000

    update geocoder

--- src/Geocoder.svelte
+++ src/Geocoder.svelte
@@ -1,9 +1,7 @@
-<div bind:this={container}>
-  <input type="text" disabled />
-</div>
+<div bind:this={container} />
 
 <script>
-  import { onMount, onDestroy, createEventDispatcher } from 'svelte'
+  import { onMount, createEventDispatcher } from 'svelte'
   import loader from '@beyonk/async-script-loader'
 
   const dispatch = createEventDispatcher()
@@ -11,10 +9,15 @@
   export let accessToken
   export let options
   export let version = 'v4.5.1'
-  export let types = [ 'country', 'region', 'postcode', 'district', 'place', 'locality', 'neighbourhood', 'address' ]
+  export let types = [ 'country', 'region', 'postcode', 'district', 'place', 'locality', 'neighborhood', 'address' ]
 
   let container
   let geocoder
+
+  const onResult = p => dispatch('result', p)
+  const onResults = p => dispatch('results', p)
+  const onError = p => dispatch('error', p)
+  const onLoading = p => dispatch('loading', p)
 
   onMount(() => {
     loader(
@@ -22,17 +25,26 @@
       () => !!window.MapboxGeocoder,
       onAvailable
     )
-  })
 
-  const onResult = p => dispatch('result', p)
-  const onResults = p => dispatch('results', p)
-  const onError = p => dispatch('error', p)
-  const onLoading = p => dispatch('loading', p)
+    const link = document.createElement('link')
+		link.rel = 'stylesheet'
+    link.href = '//api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v4.5.1/mapbox-gl-geocoder.css'
+    document.head.appendChild(link)
+
+    return () => {
+      geocoder
+        .off('results', onResults)
+        .off('result', onResult)
+        .off('loading', onLoading)
+        .off('error', onError)
+      link.parentNode.removeChild(link)
+    }
+  })
 
   function onAvailable () {
     const optionsWithDefaults = Object.assign({
         accessToken,
-        types
+        types: types.join(',')
       }, options)
     geocoder = new MapboxGeocoder(optionsWithDefaults)
     geocoder
@@ -43,21 +55,12 @@
       .on('result', onResult)
       .on('loading', onLoading)
       .on('error', onError)
+
     dispatch('ready')
   }
-
-  onDestroy(() => {
-    geocoder
-      .off('results', onResults)
-      .off('result', onResult)
-      .off('loading', onLoading)
-      .off('error', onError)
-  })
 </script>
 
 <style>
-  @import '//api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v4.5.1/mapbox-gl-geocoder.css';
-
   div {
     padding: 0;
   }
